78197235eb16b133f5703178dc539a526a91ebf9
Aleksi Mäkinen <aleksimatias.makinen@gmail.com>
2022-07-15 00:01:22 +0300

    WIP: Prelim generation file

--- finngen/__init__.py
+++ finngen/__init__.py
@@ -1,1 +1,46 @@
-__version__ = '0.1.0'
+__version__ = "0.1.0"
+
+
+from dataclasses import dataclass
+from enum import Enum
+from random import choices
+from typing import List
+
+
+class Gender(Enum, str):
+    Male = "Male"
+    Female = "Female"
+
+
+@dataclass
+class Person:
+    gender: Gender
+    first_name: str
+    middle_name: str
+    last_name: str
+
+    @property
+    def full_name(self):
+        return self.first_name + " " + self.middle_name + " " + self.last_name
+
+
+def _generate(k: int = 1) -> Person:
+
+    # It's expensive to setup choices: Initialize them as little times as possible
+    gender_choices = choices(
+        [Gender.Female, Gender.Male], [0.5, 0.5], k=k
+    )  # TODO: add sophistication
+    for gender_choice in gender_choices:
+        pass
+
+
+def generate_finnish_person() -> Person:
+    return _generate(k=1)
+
+
+def generate_finnish_people(amount: int) -> List[Person]:
+    if amount == 0:
+        return []
+    elif amount < 0:
+        raise ValueError("Cannot generate negative amount of people!")
+    return _generate(k=amount)
