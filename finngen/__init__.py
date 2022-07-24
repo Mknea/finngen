d838183a93990d150cfd110711f32af3e17a98fd
Aleksi Mäkinen <aleksimatias.makinen@gmail.com>
2022-07-24 23:16:18 +0300

    WIP: Generate residence, age and gender

--- finngen/__init__.py
+++ finngen/__init__.py
@@ -8,7 +8,7 @@ from collections import Counter
 from dataclasses import dataclass
 from enum import Enum
 from random import choices
-from typing import Iterator, List, Literal, Sequence, cast
+from typing import Iterator, List, Literal, Sequence, Tuple, cast
 
 from . import _storage
 
@@ -17,9 +17,20 @@ class Gender(str, Enum):
     Male = "Male"
     Female = "Female"
 
+    @classmethod
+    def from_str(cls, label: str):
+        if label.lower() == "male":
+            return cls.Male
+        elif label.lower() == "female":
+            return cls.Female
+        else:
+            raise NotImplementedError
+
 
 @dataclass(eq=True, frozen=True)
 class Person:
+    residence: str
+    age: int
     gender: Gender
     first_name: str
     middle_name: str
@@ -33,6 +44,7 @@ class Person:
 SOURCE_DATA = {
     key: _storage.load_data_file(f"{key}.ftr")
     for key in (
+        "location_age_gender",
         "last_names",
         "men_first_names",
         "men_middle_names",
@@ -45,12 +57,12 @@ SOURCE_DATA = {
 def _generate(k: int = 1) -> Iterator[Person]:
 
     # It's expensive to setup choices:
-    # Initialize non-gender related once, gender related once per gender
-    gender_choices = choices(
-        [Gender.Female, Gender.Male], [0.5, 0.5], k=k
-    )  # TODO: add sophistication
-    gender_choices.sort()
-    counts = Counter(gender_choices)
+    # Generate fields per dataset as few times as possible and just pair them at the end
+    residence_age_genders = _create_residence_age_gender(k)
+    residence_age_genders = sorted(
+        residence_age_genders, key=lambda x: x[2], reverse=True
+    )
+    counts_per_gender = Counter(x[2] for x in residence_age_genders)
 
     last_names: List[str] = choices(
         SOURCE_DATA["last_names"]["last_name"],
@@ -59,25 +71,36 @@ def _generate(k: int = 1) -> Iterator[Person]:
     )
     first_names: List[str] = []
     middle_names: List[str] = []
-    for gender, amount in counts.items():
-        first_names.extend(_generate_names_based_on_gender(gender, amount, "first"))
-        middle_names.extend(_generate_names_based_on_gender(gender, amount, "middle"))
+    for gender, amount in counts_per_gender.items():
+        first_names.extend(_create_names_based_on_gender(gender, amount, "first"))
+        middle_names.extend(_create_names_based_on_gender(gender, amount, "middle"))
 
-    for gender, last_name, first_name, middle_name in zip(
-        gender_choices, last_names, first_names, middle_names, strict=True
+    for (residence, age, gender), last_name, first_name, middle_name in zip(
+        residence_age_genders, last_names, first_names, middle_names, strict=True
     ):
         yield Person(
-            gender=gender,
+            residence=residence,
+            age=age,
+            gender=Gender.from_str(gender),
             first_name=first_name,
             middle_name=middle_name,
             last_name=last_name,
         )
 
 
-def _generate_names_based_on_gender(
-    gender: Gender, amount: int, name_type: Literal["first"] | Literal["middle"]
+def _create_residence_age_gender(amount: int) -> List[Tuple[str, int, str]]:
+    df = SOURCE_DATA["location_age_gender"]
+    return choices(
+        df[["area", "age", "gender"]].to_records(index=False),
+        cast(Sequence[float], df["weight"]),
+        k=amount,
+    )
+
+
+def _create_names_based_on_gender(
+    gender: str, amount: int, name_type: Literal["first"] | Literal["middle"]
 ) -> List[str]:
-    prefix = "men" if gender == Gender.Male else "women"
+    prefix = "men" if Gender.from_str(gender) == Gender.Male else "women"
     df = SOURCE_DATA[f"{prefix}_{name_type}_names"]
     return choices(
         df[f"{name_type}_name"], cast(Sequence[float], df["weight"]), k=amount
