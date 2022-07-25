1a257af2fd180b81b928f72082778909e743acf0
Aleksi Mäkinen <aleksimatias.makinen@gmail.com>
2022-07-25 21:29:23 +0300

    Fix existing proportion tests

--- tests/test_proportions.py
+++ tests/test_proportions.py
@@ -7,6 +7,12 @@ from pandas import DataFrame
 
 from finngen import Gender, Person, create_finnish_people
 
+PRESET_LOCATION_AGE_GENDER = {
+    "area": ["Akaa", "Äänekoski"],
+    "age": [20, 50],
+    "gender": ["male", "female"],
+    "weight": [0.4, 0.6],
+}
 PRESET_LAST_NAMES = {"last_name": ["McLastname", "Lasterburg"], "weight": [0.2, 0.8]}
 PRESET_MENS_FIRST_NAMES = {"first_name": ["Testy", "Batman"], "weight": [0.1, 0.9]}
 PRESET_MENS_MIDDLE_NAMES = {"middle_name": ["Middly", "MidMid"], "weight": [0.4, 0.6]}
@@ -17,19 +23,21 @@ PRESET_WOMENS_MIDDLE_NAMES = {
     "weight": [0.55, 0.45],
 }
 
+PERCENTAGE_OF_MEN_IN_SOURCE = PRESET_LOCATION_AGE_GENDER["weight"][0]  # type: ignore
+
 
 @pytest.fixture
 def mocked_source_data(monkeypatch):
-    monkeypatch.setattr(
-        "finngen.SOURCE_DATA",
-        {
-            "last_names": DataFrame.from_dict(PRESET_LAST_NAMES),
-            "men_first_names": DataFrame.from_dict(PRESET_MENS_FIRST_NAMES),
-            "men_middle_names": DataFrame.from_dict(PRESET_MENS_MIDDLE_NAMES),
-            "women_first_names": DataFrame.from_dict(PRESET_WOMENS_FIRST_NAMES),
-            "women_middle_names": DataFrame.from_dict(PRESET_WOMENS_MIDDLE_NAMES),
-        },
-    )
+    mocked_data = {
+        "location_age_gender": PRESET_LOCATION_AGE_GENDER,
+        "last_names": PRESET_LAST_NAMES,
+        "men_first_names": PRESET_MENS_FIRST_NAMES,
+        "men_middle_names": PRESET_MENS_MIDDLE_NAMES,
+        "women_first_names": PRESET_WOMENS_FIRST_NAMES,
+        "women_middle_names": PRESET_WOMENS_MIDDLE_NAMES,
+    }
+    mocked_data.update((k, DataFrame.from_dict(v)) for k, v in mocked_data.items())
+    monkeypatch.setattr("finngen.SOURCE_DATA", mocked_data)
 
 
 @pytest.fixture
@@ -42,10 +50,12 @@ def sum_counter_counts_of_values(counter, field, accepted_values):
     return sum([count for val, count in counter.items() if getattr(val, field) in accepted_values])
 
 
-def test_gender_is_assigned_in_equal_proportion(generated_finns: List[Person]):
+# FIXME: Refactor mocked data access
+def test_gender_is_assigned_in_set_proportions(generated_finns: List[Person]):
     counts = Counter(generated_finns)
-    count_of_men = sum_counter_counts_of_values(counts, "gender", Gender.Male)
-    assert count_of_men == pytest.approx(len(generated_finns) / 2, rel=5e-3)
+    count_of_generated_men = sum_counter_counts_of_values(counts, "gender", Gender.Male)
+    expected_amount_of_men = len(generated_finns) * PERCENTAGE_OF_MEN_IN_SOURCE
+    assert count_of_generated_men == pytest.approx(expected_amount_of_men, rel=1e-1)
 
 
 def test_names_are_assigned_in_set_proportions(generated_finns: List[Person]):
@@ -54,7 +64,9 @@ def test_names_are_assigned_in_set_proportions(generated_finns: List[Person]):
         counts, "first_name", [PRESET_MENS_FIRST_NAMES["first_name"][0]]  # type: ignore
     )
     assert count_of_specific_men_first_name == pytest.approx(
-        len(generated_finns) * 0.5 * PRESET_MENS_FIRST_NAMES["weight"][0],  # type: ignore
+        len(generated_finns)
+        * PERCENTAGE_OF_MEN_IN_SOURCE
+        * PRESET_MENS_FIRST_NAMES["weight"][0],  # type: ignore
         rel=1e-1,
     )
 
@@ -64,6 +76,11 @@ def test_names_are_assigned_in_set_proportions(generated_finns: List[Person]):
         [PRESET_WOMENS_MIDDLE_NAMES["middle_name"][1]],  # type: ignore
     )
     assert count_of_specific_women_middle_name == pytest.approx(
-        len(generated_finns) * 0.5 * PRESET_WOMENS_MIDDLE_NAMES["weight"][1],  # type: ignore
+        len(generated_finns)
+        * (1 - PERCENTAGE_OF_MEN_IN_SOURCE)
+        * PRESET_WOMENS_MIDDLE_NAMES["weight"][1],  # type: ignore
         rel=1e-1,
     )
+
+
+# TODO: Test all assigned names conform to the gender
