61770be8e322e044d9376619d3d02961bdb6b6c4
Aleksi Mäkinen <aleksimatias.makinen@gmail.com>
2024-03-08 18:11:12 +0200

    Check if revised implementation fixed test consideration

--- tests/test__storage.py
+++ tests/test__storage.py
@@ -1,9 +1,7 @@
-import sys
 from pathlib import Path
 from unittest.mock import Mock, patch
 
 import pytest
-from importlib_resources import path
 
 from finngen._storage import load_data_file
 from finngen.constants import SOURCE_FILES_TO_COLUMNS
@@ -12,13 +10,7 @@ from finngen.constants import SOURCE_FILES_TO_COLUMNS
 @patch("pandas.read_feather")
 def test_load_data_file(mocked_read_feather: Mock) -> None:
     fake_file = "this-does-not-actually-exist.asd"
-    if sys.version_info < (3, 10):
-        # Behaviour changes between versions, patch to backported version
-        # https://bugs.python.org/issue44137
-        with patch("finngen._storage.path", side_effect=path):
-            load_data_file(fake_file)
-    else:
-        load_data_file(fake_file)
+    load_data_file(fake_file)
     mocked_read_feather.assert_called_once_with(Path.cwd() / "finngen" / "data" / fake_file)
 
 
